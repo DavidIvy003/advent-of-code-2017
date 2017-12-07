99bed09dfc9f7e3da1c1b920e1ad15548117626c
David Ivy <Davidivy003@gmail.com>
2017-12-07 00:31:42 -0500

    Third day complete: spiral memory

---
+++ day_03/spiral_memory.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env bundle exec ruby
+
+class SpiralMemory
+  attr_accessor :number
+
+  def initialize(number)
+    @number = number
+  end
+
+  def result
+    x, y = grid_position(@number)
+    x.abs + y.abs
+  end
+
+  def grid_position(number)
+    direction = :down
+    x = 0
+    y = 0
+    grid = {}
+    number -= 1
+    number.times do |num|
+      grid["#{x},#{y}"] = num
+      direction = adjust_direction(x, y, direction, grid)
+      x, y = move_coordinates(x, y, direction)
+    end
+    [x,y]
+  end
+
+  def move_coordinates(x, y, direction)
+    case direction
+    when :left
+      x -= 1
+    when :right
+      x += 1
+    when :up
+      y += 1
+    when :down
+      y -= 1
+    end
+    [x, y]
+  end
+
+  def adjust_direction(x, y, direction, grid)
+    case direction
+    when :left
+      direction = :down unless grid["#{x},#{y - 1}"]
+    when :right
+      direction = :up unless grid["#{x},#{y + 1}"]
+    when :up
+      direction = :left unless grid["#{x - 1},#{y}"]
+    when :down
+      direction = :right unless grid["#{x + 1},#{y}"]
+    end
+    direction
+  end
+end
+
+if __FILE__ == $0
+  # this will only run if the script was the main, not load'd or require'd
+  puts InverseCaptcha.new( ARGV[0] ).result
+end
---
+++ day_03/spiral_memory_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require './spiral_memory'
+
+describe SpiralMemory do
+  it 'returns the correct response' do
+    SpiralMemory.new(1).result.must_equal 0
+    SpiralMemory.new(12).result.must_equal 3
+    SpiralMemory.new(23).result.must_equal 2
+    SpiralMemory.new(1024).result.must_equal 31
+  end
+end
