f7c7edaab8fa47aa95ff819efdef51b8835a7eff
David Ivy <davidivy003@gmail.com>
2017-12-15 18:25:35 -0500

    Day 1 part 2 complete: inverse captcha half way through string

--- day_01/inverse_captcha.rb
+++ day_01/inverse_captcha.rb
@@ -1,10 +1,11 @@
 #!/usr/bin/env bundle exec ruby
 
 class InverseCaptcha
-  attr_accessor :digits
+  attr_accessor :digits, :increment
 
   def initialize(number, half_way = false)
     @digits = number.to_s.split('')
+    @increment = half_way ? digits.length / 2 : 1
   end
 
   def result
@@ -23,11 +24,15 @@ class InverseCaptcha
   end
 
   private def next_digit(index)
-    index + 1 < digits.length ? digits[index + 1] : digits.first
+    position = index + increment
+    if index + increment >= digits.length
+      position = index + increment - digits.length
+    end
+    digits[position]
   end
 end
 
 if __FILE__ == $0
   # this will only run if the script was the main, not load'd or require'd
-  puts InverseCaptcha.new( ARGV[0] ).result
+  puts InverseCaptcha.new( ARGV[0], ARGV[1] == 'true' ).result
 end
