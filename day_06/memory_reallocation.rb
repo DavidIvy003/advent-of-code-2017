4aac222db981c305a2278c217cd893e3a98c514c
David Ivy <Davidivy003@gmail.com>
2017-12-17 18:57:13 -0500

    Sixth day part 2 complete: steps between memory reallocation repeats

--- day_06/memory_reallocation.rb
+++ day_06/memory_reallocation.rb
@@ -8,26 +8,33 @@ class MemoryReallocation
     @seen_memory = {}
   end
 
-  def result
-    memory = @memory
-    counter = 0
+  def steps
+    allocate_memory(memory)[0]
+  end
+
+  def steps_between_repeats
+    allocate_memory(memory)[1]
+  end
+
+  private def allocate_memory(memory)
+    step = 0
     while !has_seen(memory)
-      counter += 1
-      add_to_seen(memory)
+      step += 1
+      add_to_seen(memory, step)
       max_bank_index = memory.index(memory.max)
       count = memory[max_bank_index]
       memory[max_bank_index] = 0
       memory = redistribute_memory(memory, count, max_bank_index)
     end
-    counter
+    [step, step - seen_memory[memory.join('')] + 1]
   end
 
   private def has_seen(memory)
     seen_memory[memory.join('')]
   end
 
-  private def add_to_seen(memory)
-    seen_memory[memory.join('')] = true
+  private def add_to_seen(memory, step)
+    seen_memory[memory.join('')] = step unless has_seen(memory)
   end
 
   private def redistribute_memory(memory, count, current_index)
@@ -41,5 +48,5 @@ end
 
 if __FILE__ == $0
   # this will only run if the script was the main, not load'd or require'd
-  puts MemoryReallocation.new( ARGV[0] ).result
+  puts MemoryReallocation.new( ARGV[0] ).steps
 end
--- day_06/memory_reallocation_test.rb
+++ day_06/memory_reallocation_test.rb
@@ -3,8 +3,14 @@ require './memory_reallocation'
 
 describe MemoryReallocation do
   it 'returns the correct response' do
-    MemoryReallocation.new([0, 0, 0, 0, 0]).result.must_equal 1
-    MemoryReallocation.new([0, 2, 7, 0]).result.must_equal 5
-    MemoryReallocation.new([4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]).result.must_equal 6681
+    MemoryReallocation.new([0, 0, 0, 0, 0]).steps.must_equal 1
+    MemoryReallocation.new([0, 2, 7, 0]).steps.must_equal 5
+    MemoryReallocation.new([4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]).steps.must_equal 6681
+  end
+
+  it 'returns the correct response' do
+    MemoryReallocation.new([0, 0, 0, 0, 0]).steps_between_repeats.must_equal 1
+    MemoryReallocation.new([0, 2, 7, 0]).steps_between_repeats.must_equal 4
+    MemoryReallocation.new([4, 1, 15, 12, 0, 9, 9, 5, 5, 8, 7, 3, 14, 5, 12, 3]).steps_between_repeats.must_equal 2392
   end
 end
