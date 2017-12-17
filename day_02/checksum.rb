819425ea0738a3fd6ceb1c0b190f47c6b6c9e799
David Ivy <Davidivy003@gmail.com>
2017-12-17 00:17:37 -0500

    Second day part 2 complete: divisible checksum

--- day_02/checksum.rb
+++ day_02/checksum.rb
@@ -1,26 +1,49 @@
 #!/usr/bin/env bundle exec ruby
 
 class Checksum
-  attr_accessor :rows
+  attr_accessor :rows, :method
 
-  def initialize(filename)
+  def initialize(filename, method='MAX_MIN')
     file = File.read(filename)
     @rows = file.split("\n")
+    @method = method
   end
 
   def result
     sum = 0
     rows.each do |row|
       columns = row.split(' ').map(&:to_f)
-      highest = columns.max
-      lowest = columns.min
-      sum += highest - lowest
+      sum += rowChecksum(columns)
     end
     sum
+  end
+
+  private def rowChecksum(columns)
+    if method == 'MAX_MIN'
+      maxMinChecksum(columns)
+    elsif method == 'DIVISIBLE'
+      divisibleChecksum(columns)
+    end
+  end
+
+  private def maxMinChecksum(columns)
+    highest = columns.max
+    lowest = columns.min
+    highest - lowest
+  end
+
+  private def divisibleChecksum(columns)
+    numerator = nil
+    denominator = columns.find do |denominator|
+      numerator = columns.find do |numerator|
+        denominator != numerator && numerator % denominator == 0
+      end
+    end
+    numerator / denominator
   end
 end
 
 if __FILE__ == $0
   # this will only run if the script was the main, not load'd or require'd
-  puts InverseCaptcha.new( ARGV[0] ).result
+  puts Checksum.new( ARGV[0] ).result
 end
--- day_02/checksum_test.rb
+++ day_02/checksum_test.rb
@@ -2,8 +2,14 @@ require 'minitest/autorun'
 require './checksum'
 
 describe Checksum do
-  it 'returns the correct response' do
+  it 'max min checksum' do
     Checksum.new('input/example_01.txt').result.must_equal 18
     Checksum.new('input/example_02.txt').result.must_equal 492
+    Checksum.new('input/example_03.txt').result.must_equal 45351
+  end
+
+  it 'divisible checksum' do
+    Checksum.new('input/example_04.txt', 'DIVISIBLE').result.must_equal 9
+    Checksum.new('input/example_03.txt', 'DIVISIBLE').result.must_equal 275
   end
 end
