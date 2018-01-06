0e7a9b94cec20b34d39599d96f46fdad1afb10c4
David Ivy <Davidivy003@gmail.com>
2018-01-05 19:14:58 -0500

    Complete day 10 part2: knot hash algorithm

--- day_10/knotHash.js
+++ day_10/knotHash.js
@@ -16,27 +16,64 @@ const reverseListSection = (list, currentPosition, length) => {
   }
 };
 
-const knotHash = (input, size = 256) => {
-  const lengths = input.split(',');
-  let list = Array.from(Array(size).keys());
-  let skipSize = 0;
-  let currentPosition = 0;
+const evaluateRound = (lengths, sparseHash, currentPosition = 0, skipSize = 0) => {
   lengths.forEach((length) => {
     // console.log(`length=${length}`)
     // console.log(`currentPosition=${currentPosition}`)
     // console.log(`skipSize=${skipSize}`)
     // console.log(`input list=${list.toString()}`)
-    list = reverseListSection(list, currentPosition, +length);
+    sparseHash = reverseListSection(sparseHash, currentPosition, +length);
     // console.log(`output list=${list.toString()}`)
-    currentPosition = (currentPosition + +length + skipSize) % list.length;
+    currentPosition = (currentPosition + +length + skipSize) % sparseHash.length;
     skipSize++;
   });
-  return list;
+  return {
+    sparseHash,
+    currentPosition,
+    skipSize,
+  };
 };
 
+const knotHash = (input, size = 256) => {
+  let sparseHash = Array.from(Array(size).keys());
+  let lengths = encode(input);
+  let skipSize = 0;
+  let currentPosition = 0;
+  for (i = 0; i < 64; i++) {
+    const output = evaluateRound(lengths, sparseHash, currentPosition, skipSize);
+    sparseHash = output.sparseHash;
+    skipSize = output.skipSize;
+    currentPosition = output.currentPosition;
+  }
+
+  return convertToHexadecimal(getDenseHash(sparseHash));
+};
+
+const xOr = (input) => {
+  let result;
+  for (let i = 0; i < 16; i++) {
+    result = result ^ input[i];
+  }
+  return result;
+};
+
+const getDenseHash = (sparseHash) => {
+  let result = [];
+  for (i = 0; i < sparseHash.length; i += 16) {
+    result.push(xOr(sparseHash.slice(i, i + 16)));
+  }
+  return result;
+};
+
+const hexadecimal = (num) => (num.length === 2 ? num : `0${num}`)
+const convertToHexadecimal = (array) => array.map(num => hexadecimal(num.toString(16))).join('');
+const encode = (input) => [...input.split('').map(c => c.charCodeAt(0)), 17, 31, 73, 47, 23];
 const verify = (list) => list[0] * list[1];
 
 module.exports = {
+  encode,
+  evaluateRound,
+  getDenseHash,
   knotHash,
   reverseListSection,
   verify,
--- day_10/knotHash.test.js
+++ day_10/knotHash.test.js
@@ -2,15 +2,41 @@ const expect = require('chai').expect;
 const fs = require('fs');
 const KnotHash = require('./knotHash');
 
-const { knotHash, verify } = KnotHash;
+const {
+  encode,
+  evaluateRound,
+  getDenseHash,
+  knotHash,
+  verify,
+} = KnotHash;
 
 describe('KnotHash', () => {
   it('evaluates the hash', () => {
-    expect(knotHash('3, 4, 1, 5', 5)).to.deep.equal([3, 4, 2, 1, 0]);
-    expect(verify(knotHash('3, 4, 1, 5', 5))).to.equal(12);
+    const list = Array.from(Array(5).keys());
+    expect(evaluateRound([3, 4, 1, 5], list).sparseHash).to.deep.equal([3, 4, 2, 1, 0]);
+    expect(verify(evaluateRound([3, 4, 1, 5], list).sparseHash)).to.equal(12);
   });
 
   it('evaluates the hash from puzzle input', () => {
-    expect(verify(knotHash('106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36'))).to.equal(11413);
+    const list = Array.from(Array(256).keys());
+    expect(verify(evaluateRound([106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36], list).sparseHash)).to.equal(11413);
+  });
+
+  it('encodes the input', () => {
+    expect(encode('1,2,3')).to.deep.equal([49,44,50,44,51,17,31,73,47,23]);
+  });
+
+  it('creates the dense hash', () => {
+    const array = [65, 27, 9, 1, 4, 3, 40, 50, 91, 7, 6, 0, 2, 5, 68, 22];
+    expect(getDenseHash(array)).to.deep.equal([64]);
+    expect(getDenseHash([...array, ...array, ...array, ...array])).to.deep.equal([64, 64, 64, 64]);
+  });
+
+  it('evaluates the knot hash', () => {
+    expect(knotHash('')).to.equal('a2582a3a0e66e6e86e3812dcb672a272');
+    expect(knotHash('AoC 2017')).to.equal('33efeb34ea91902bb2f59c9920caa6cd');
+    expect(knotHash('1,2,3')).to.equal('3efbe78a8d82f29979031a4aa0b16a9d');
+    expect(knotHash('1,2,4')).to.equal('63960835bcdc130f0b66d7ff4f6a5a8e');
+    expect(knotHash('106,16,254,226,55,2,1,166,177,247,93,0,255,228,60,36')).to.equal('63960835bcdc130f0b66d7ff4f6a5a8e');
   });
 });
