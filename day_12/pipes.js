6abee1392684f09519db5d53e1b2b143c01dfe8d
David Ivy <Davidivy003@gmail.com>
2018-01-13 00:29:35 -0500

    Complete day 12 part2: count communication groups

--- day_12/pipes.js
+++ day_12/pipes.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const flattenArray = list =>
   list.reduce((a, b) => a.concat(Array.isArray(b) ? flattenArray(b) : b), []);
 
-var uniqueArray = (arrArg) =>
+const uniqueArray = arrArg =>
   arrArg.filter((elem, pos, arr) => arr.indexOf(elem) == pos);
 
 const getConnections = (programs, programId, connections = []) => {
@@ -11,7 +11,7 @@ const getConnections = (programs, programId, connections = []) => {
     return programs[programId];
   }
   connections.push(programId);
-  return flattenArray(programs[programId].map((id) => getConnections(programs, id, connections)));
+  return uniqueArray(flattenArray(programs[programId].map((id) => getConnections(programs, id, connections))));
 };
 
 const getProgramsAndConnections = (input) => {
@@ -23,12 +23,34 @@ const getProgramsAndConnections = (input) => {
   return programs;
 };
 
+const getCommunicationGroups = (programs) => {
+  let groups = {};
+  let seenProgramIds = [];
+  Object.keys(programs).forEach((programId) => {
+    if (seenProgramIds.includes(programId)) return;
+    const connections = getConnections(programs, programId);
+    seenProgramIds = [
+      ...seenProgramIds,
+      ...connections,
+    ];
+    groups[programId] = connections;
+  });
+  return groups;
+};
+
 const getCommunicationGroup = (filename, programId) => {
   const input = fs.readFileSync(filename).toString().trim();
   const programs = getProgramsAndConnections(input);
-  return uniqueArray(getConnections(programs, programId));
+  return getConnections(programs, programId);
+};
+
+const countCommunicationGroups = (filename) => {
+  const input = fs.readFileSync(filename).toString().trim();
+  const programs = getProgramsAndConnections(input);
+  return Object.keys(getCommunicationGroups(programs)).length;
 };
 
 module.exports = {
+  countCommunicationGroups,
   getCommunicationGroup,
 };
--- day_12/pipes.test.js
+++ day_12/pipes.test.js
@@ -2,6 +2,7 @@ const expect = require('chai').expect;
 const Pipes = require('./pipes');
 
 const {
+  countCommunicationGroups,
   getCommunicationGroup,
 } = Pipes;
 
@@ -9,5 +10,10 @@ describe('Pipes', () => {
   it('returns the communication group', () => {
     expect(getCommunicationGroup('input/example_01.txt', '0').length).to.equal(6);
     expect(getCommunicationGroup('input/example_02.txt', '0').length).to.equal(175);
+  });
+
+  it('returns the number of groups', () => {
+    expect(countCommunicationGroups('input/example_01.txt')).to.equal(2);
+    expect(countCommunicationGroups('input/example_02.txt')).to.equal(213);
   });
 });
