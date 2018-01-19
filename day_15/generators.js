fde5e64f91326e1004abb4126520d960614b677c
David Ivy <Davidivy003@gmail.com>
2018-01-18 23:36:10 -0500

    Complete day 15 part2: independent dueling generators

--- day_15/generators.js
+++ day_15/generators.js
@@ -2,18 +2,33 @@
 const MOD = 2147483647;
 const GENERATOR_A_FACTOR = 16807;
 const GENERATOR_B_FACTOR = 48271;
+const GENERATOR_A_MULTIPLE = 4;
+const GENERATOR_B_MULTIPLE = 8;
+
 
 const toBinary = (input) => input.toString(2);
 const padLeft = (input, length) => ('0'.repeat(length) + input).slice(-length);
 const lowest16Bits = (input) => padLeft(toBinary(input), 16).slice(-16);
 
 const valuesMatch = (valueA, valueB) => lowest16Bits(valueA) === lowest16Bits(valueB);
+const getGeneatorValue = (input, factor) => (input * factor) % MOD;
 
-const countMatchedSequences = (generatorAValue, generatorBValue) => {
+const getNextGeneratorValue = (input, factor, multiple, independentGenerators) => {
+  let value = getGeneatorValue(input, factor);
+  if (!independentGenerators) return value;
+
+  while (value % multiple !== 0) {
+    value = getGeneatorValue(value, factor);
+  }
+  return value;
+};
+
+const countMatchedSequences = (generatorAValue, generatorBValue, independentGenerators = false) => {
   let countMatches = 0;
-  for (let i = 0; i < 40000000; i++) {
-    generatorAValue = (generatorAValue * GENERATOR_A_FACTOR) % MOD;
-    generatorBValue = (generatorBValue * GENERATOR_B_FACTOR) % MOD;
+  const numberOfTrials = independentGenerators ? 5000000 : 40000000;
+  for (let i = 0; i < numberOfTrials; i++) {
+    generatorAValue = getNextGeneratorValue(generatorAValue, GENERATOR_A_FACTOR, GENERATOR_A_MULTIPLE, independentGenerators);
+    generatorBValue = getNextGeneratorValue(generatorBValue, GENERATOR_B_FACTOR, GENERATOR_B_MULTIPLE, independentGenerators);
     if (valuesMatch(generatorAValue, generatorBValue)) countMatches++;
   }
   return countMatches;
--- day_15/generators.test.js
+++ day_15/generators.test.js
@@ -10,4 +10,9 @@ describe('Generators', () => {
     expect(countMatchedSequences(65, 8921)).to.equal(588);
     expect(countMatchedSequences(277, 349)).to.equal(592);
   }).timeout(30000); // Takes a long time to run
+
+  it('counts the matched sequences for independent generators', () => {
+    expect(countMatchedSequences(65, 8921, true)).to.equal(309);
+    expect(countMatchedSequences(277, 349, true)).to.equal(320);
+  }).timeout(30000); // Takes a long time to run
 });
