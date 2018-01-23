89662347ef3b70146c0623358dcceb82c44a691d
David Ivy <Davidivy003@gmail.com>
2018-01-23 00:09:29 -0500

    Complete day 17 part2: spinlock after zero

--- day_17/spinlock.js
+++ day_17/spinlock.js
@@ -1,13 +1,27 @@
-const shortCircuit = (input) => {
+const valueAfterLast = (input, rotations = 2017) => {
+  const valueAfter = rotations;
   let buffer = [0];
   let currentPosition = 0;
-  for (let i = 1; i <= 2017; i++) {
+  for (let i = 1; i <= rotations; i++) {
     currentPosition = (currentPosition + input) % buffer.length + 1;
     buffer.splice(currentPosition, 0, i);
   }
-  return buffer[buffer.indexOf(2017) + 1];
+  return buffer[buffer.indexOf(valueAfter) + 1];
+};
+
+const valueAfterFirst = (input, rotations = 2017) => {
+  let length = 1;
+  let value;
+  let currentPosition = 0;
+  for (let i = 1; i <= rotations; i++) {
+    currentPosition = (currentPosition + input) % length + 1;
+    if (currentPosition === 1) value = i;
+    length++;
+  }
+  return value;
 };
 
 module.exports = {
-  shortCircuit,
+  valueAfterLast,
+  valueAfterFirst,
 };
--- day_17/spinlock.test.js
+++ day_17/spinlock.test.js
@@ -4,12 +4,20 @@ const fs = require('fs');
 const Spinlock = require('./spinlock');
 
 const {
-  shortCircuit,
+  valueAfterFirst,
+  valueAfterLast,
 } = Spinlock;
 
 describe('Spinlock', () => {
   it('processes instruction', () => {
-    expect(shortCircuit(3)).to.equal(638);
-    expect(shortCircuit(363)).to.equal(136);
+    expect(valueAfterLast(3)).to.equal(638);
+    expect(valueAfterLast(363)).to.equal(136);
+    expect(valueAfterLast(3, 5)).to.equal(2);
+  });
+
+  it('processes instruction', () => {
+    expect(valueAfterFirst(3)).to.equal(1226);
+    expect(valueAfterFirst(363, 50000000)).to.equal(1080289);
+    expect(valueAfterFirst(3, 2017)).to.equal(1226);
   });
 });
